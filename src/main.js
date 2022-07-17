5b323b691b6924688fb5bb4146063c8d054482f3
Akele Belay <akelebch12@gmail.com>
2022-07-17 13:35:03 +0000

    My First Commit

---
+++ src/main.js
@@ -0,0 +1,43 @@
+const message = 'Hello world' // Try edit me
+const title ='Akele Belay Walelign'
+
+// Update header text
+document.querySelector('#header').innerHTML = message
+document.getElementById('titl').innerHTML = title
+
+// Log to console
+console.log(message)
+
+// variables
+
+// let a;
+// a = 1997
+// a = "Akele"
+// console.log(typeof a)
+
+/* MyFunc = () => {
+    document.getElementById("bio").innerHTML = "my name is Akele Belay Walelgign"
+    console.log("my name is Akele Belay Walelgign")
+    let birthYear = 1997
+    console.log(birthYear)
+    var year=1998
+} */
+//MyFunc()
+// console.log(year)
+
+// var age = 100;
+//   if (age > 12){
+//     var dogYears = age * 7;
+//     console.log(`You are ${dogYears} dog years old!`);
+// }
+//   console.log(dogYears) it works because var is a function scoped not block scoped
+
+// var age = 100;
+//   if (age > 12){
+//     let dogYears = age * 7;
+//     //console.log("You are", dogYears, "dog years old!");
+//     console.log(`You are ${dogYears} dog years old!`);
+// }
+  //console.log(dogYears) err- let is a blockscoped 
+
+
