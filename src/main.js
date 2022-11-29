11e3e3570417b726e5baf0fc40321d704323f874
Akele Belay <akelebch12@gmail.com>
2022-11-29 09:26:14 +0000

    commit test

--- src/main.js
+++ src/main.js
@@ -1,26 +1,25 @@
-/* eslint-disable no-console */
-const message = 'Hello world'; // Try edit me
-const title = 'Akele Belay Walelign';
-// Update header text
-document.querySelector('#header').innerHTML = message;
-document.getElementById('titl').innerHTML = title;
 
-console.log(message);
+// const message = 'Hello world'; // Try edit me
+// const title = 'Akele Belay Walelign';
+// // Update header text
+// document.querySelector('#header').innerHTML = message;
+// document.getElementById('title').innerHTML = title;
+
+// console.log(message);
 
 // variables
-
 // let a;
-// a = 1997
-// a = "Akele"
-// console.log(typeof a)
+// a = 1997;
+// a = "Akele";
+// console.log(typeof a);
 
-/* MyFunc = () => {
-    document.getElementById("bio").innerHTML = "my name is Akele Belay Walelgign"
-    console.log("my name is Akele Belay Walelgign")
-    let birthYear = 1997
-    console.log(birthYear)
-    var year=1998
-} */
+//  MyFunc = () => {
+//     document.getElementById("bio").innerHTML = "my name is Akele Belay Walelgign"
+//     console.log("my name is Akele Belay Walelgign")
+//     let birthYear = 1997
+//     console.log(birthYear)
+//     var year=1998
+// } 
 // MyFunc()
 // console.log(year)
 
